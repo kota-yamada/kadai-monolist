ef27c4a005cb79d770201f2f4344ead58e785dd8
kota.yamada <ec2-user@ip-172-31-5-57.us-east-2.compute.internal>
2018-06-19 03:07:48 +0000

    have

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 use \App\Item;
 
   class ItemsController extends Controller
@@ -40,10 +44,12 @@ use \App\Item;
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->have_users;
 
       return view('items.show', [
           'item' => $item,
           'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
     }
     
--- resources/views/items/want_button.blade.php
+++ resources/views/items/want_button.blade.php
@@ -1,7 +1,7 @@
 @if (Auth::user()->is_wanting($item->code))
     {!! Form::open(['route' => 'item_user.dont_want', 'method' => 'delete']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
-        {!! Form::submit('Want', ['class' => 'btn btn-success']) !!}
+        {!! Form::submit('Unwanted', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => 'item_user.want']) !!}
